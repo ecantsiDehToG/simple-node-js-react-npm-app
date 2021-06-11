179bbb5a349a96c68c95e4172061a2e06a5a6255
ecantsiDehToG <48802425+ecantsiDehToG@users.noreply.github.com>
2021-06-11 18:14:04 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-<a href="http://www.w3school.com.cn">CPS回调</a>
+<a href="https://testjdh5insure.jxiaobao.cn/api/v1/notify/smartQuestionCallback?orderNo=2021061117552684357813&channelCode=100766&customer=2&encryptOrderNo=sMZM1V1TrQ93ng
+pxbs6fGbKKuJzuRVnO0KyQ9ysMg2o%3D&successPageCode=af2b1368">CPS回调</a>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
