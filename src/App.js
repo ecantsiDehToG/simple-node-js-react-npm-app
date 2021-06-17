1b30fcf59a40ed6ea80b736755573499b7c8d429
ecantsiDehToG <48802425+ecantsiDehToG@users.noreply.github.com>
2021-06-17 16:43:00 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,15 +2,39 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const getUrlParam = name => {
+  var reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)');
+  var r = window.location.search.substr(1).match(reg);
+  if (r != null) return unescape(r[2]);
+  return null;
+};
+
 class App extends Component {
+  constructor(props){
+            super(props);
+        this.state={
+          sUrl:'',
+          fUrl:'',
+        }
+      }
+  componentDidMount() {
+    const sUrl=decodeURIComponent(getUrlParam('sUrl'));
+    const fUrl=decodeURIComponent(getUrlParam('fUrl'));
+    this.setState({
+      sUrl,
+      fUrl,
+    })
+  }
+  
+  const a=1
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
-<a href="https://testjdh5insure.jxiaobao.cn/api/v1/notify/smartQuestionCallback?orderNo=2021061117552684357813&channelCode=100766&customer=2&encryptOrderNo=sMZM1V1TrQ93ng
-pxbs6fGbKKuJzuRVnO0KyQ9ysMg2o%3D&successPageCode=af2b1368">彭杰文CPS回调地址测试</a>
+          <a href={this.state.sUrl}>CPS 成功 回调地址测试</a>
+          <a href={this.state.fUrl}>CPS 失败 回调地址测试</a>
         </header>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
